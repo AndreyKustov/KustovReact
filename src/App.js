a66254105c15a1ddd7d13af0132f725b0329d126
Andrey <andreykustov26@gmail.com>
2018-07-07 11:44:13 +0500

    работа по 1 уроку интенсива

--- src/App.js
+++ src/App.js
@@ -2,26 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Grid from 'react-bootstrap/lib/Button';
-import Row from 'react-bootstrap/lib/Button';
-import Col from 'react-bootstrap/lib/Button';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
 
 import styled from 'styled-components';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+            <div className ="container">
+              <Hello />
+            </div>
     );
   }
+}
+
+function Hello () {
+  let phrase = "Academy"
+  return (
+    <div className ="main-block text-center">
+      <h1>Hello {phrase}!</h1>
+      <SayFullName name="Andrey" surname="Kustov" link="https://vk.com/id85930382"/>
+    </div>
+  )
+}
+
+function SayFullName (props) {
+  return (
+          <div className ="about">
+            <h2> Мое имя {props.name}, фамилия - {props.surname}</h2>
+            <a href={props.link}> Ссылка на мой профиль</a>
+          </div>
+  )
 }
 
 export default App;
